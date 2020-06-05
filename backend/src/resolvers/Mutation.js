2fb8e5f3a30f35c62e56b7acf1f2752195d3b1a4
Sam Neely <sneely@samneely.org>
2020-06-05 11:08:09 -0400

    Item mutation and query setup

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,11 +1,14 @@
 const mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    // create a dog
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
-  },
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
 };
 
 module.exports = mutations;
