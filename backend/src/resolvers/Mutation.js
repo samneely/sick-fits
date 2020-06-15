c88943351f354935d9318a3e515303f9abd5e7c3
Sam Neely <sneely@samneely.org>
2020-06-15 15:48:25 -0400

    delete and update item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -8,6 +8,28 @@ const mutations = {
     }, info);
 
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    //remote the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id,
+      }
+    }, info);
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // 2. check if they own that item or have the permissions
+    // TODO
+    // 3. delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
